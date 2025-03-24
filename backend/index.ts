716d6d3a8473c9b9c899cb6164250daefddf7a79
Beacesconetto <beacesconetto203@hotmail.com>
2025-03-24 16:17:18 -0300

    feat: scraping web page Mercado Livre

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
 import cors from 'cors';
-
 
 const app = express();
 const PORT = 3000;
@@ -13,7 +14,49 @@ app.get('/api/scrape', async (req: any, res: any) => {
   if (!keyword) {
     return res.status(400).json({ error: 'Keyword is required' });
   }
-  return res.status(200).json({ message: keyword });
+  try {
+    const url = `https://lista.mercadolivre.com.br/${encodeURIComponent(keyword)}#D[A:${encodeURIComponent(keyword)}]`;
+    const { data } = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept': 'text/html',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Cache-Control': 'max-age=0',
+      },
+    })
+
+    const dom = new JSDOM(data);
+    const document = dom.window.document;
+
+
+    const items = document.querySelectorAll('.ui-search-layout__item');
+
+    const results: any[] = [];
+
+    console.log('items', items.length)
+
+    for (const item of items) {
+      const title = item.querySelector('.poly-component__title')?.textContent?.trim();
+      const price = item.querySelector('.poly-price__current')?.textContent?.trim();
+      const link = item.querySelector('.poly-component__title')?.getAttribute('href');
+      const image = item.querySelector('.poly-component__picture')?.getAttribute('src');
+
+      if (title && price && link && image) {
+        results.push({ title, price, link, image });
+      } else {
+        console.log("Item não completo ou não encontrado, pulando.");
+      }
+    }
+
+    return res.json(results);
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Failed to scrape data' });
+  }
 });
 
 app.listen(PORT, () => {
